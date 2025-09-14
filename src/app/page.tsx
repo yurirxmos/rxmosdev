0b0a599bb15298aad2a11c2f6511da049d333810
Yuri Ramos <yuriramos2406@gmail.com>
2025-09-14 12:20:51 -0300

    refactor: code structure for improved readability and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { ChevronRight } from "lucide-react";
 import { CgAttachment } from "react-icons/cg";
 import { FaChevronDown, FaExternalLinkAlt, FaInbox, FaMailBulk, FaMapPin } from "react-icons/fa";
 import { TbBrandMailgun, TbMailFast } from "react-icons/tb";
+import all4chat from "../../public/favicon.png";
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -46,50 +47,82 @@ export default function Home() {
         <div className="flex flex-col items-center text-center gap-5">
           <p className="text-sm max-w-md font-black mb-10 uppercase">You think, We build!</p>
 
-          <div className="flex flex-col items-start text-start gap-4 w-full">
-            <p className="tracking-[25px] text-xs">OUR CUSTOMERS</p>
+          <div className="flex flex-col gap-10 items-start text-center">
+            <div className="flex flex-col items-start text-start gap-4 w-full">
+              <p className="tracking-[15px] text-xs">OUR PRODUCTS</p>
 
-            <div className="flex flex-row justify-start gap-10 items-center text-start w-full">
-              <div className="flex flex-row items-center gap-4">
-                <div className="relative flex flex-row gap-2 w-15 h-15 rounded-tl-3xl rounded-br-3xl overflow-hidden flex-shrink-0">
-                  <Image
-                    src={vizantu}
-                    alt="vizantu Logo"
-                    fill
-                    className="object-cover saturate-0"
-                  />
+              <div className="flex flex-row justify-start gap-10 items-center text-start w-full">
+                <div className="flex flex-row items-center gap-4">
+                  <div className="relative flex flex-row gap-2 w-15 h-15 rounded-tl-3xl rounded-br-3xl overflow-hidden flex-shrink-0">
+                    <Image
+                      src={all4chat}
+                      alt="All4Chat Logo"
+                      fill
+                      className="object-cover saturate-0"
+                    />
+                  </div>
+                  <div>
+                    <h1 className="text-md font-bold">All4Chat</h1>
+                    <h2 className="text-xs text-muted-foreground w-fit">Tool for Streamers</h2>
+                    <p className="text-xs text-muted-foreground">Product since 2025</p>
+                  </div>
                 </div>
-                <div>
-                  <h1 className="text-md font-bold">Vizantu</h1>
-                  <h2 className="text-xs text-muted-foreground w-fit">Social Media</h2>
-                  <p className="text-xs text-muted-foreground">Customer since 2021</p>
-                </div>
+
+                <a
+                  href="https://all4chat.rxmos.dev.br"
+                  target="_blank"
+                  className="text-xs text-muted-foreground"
+                >
+                  <FaExternalLinkAlt />
+                </a>
               </div>
-
-              <a
-                href="https://vizantu.com.br"
-                target="_blank"
-                className="text-xs text-muted-foreground"
-              >
-                <FaExternalLinkAlt />
-              </a>
             </div>
-          </div>
 
-          <div className="flex flex-col items-start text-start gap-3 w-full mt-10">
-            <p className="tracking-[25px] text-xs">CONTACT</p>
-            <div className="flex flex-col  items-start text-start gap-2">
-              <a
-                href="mailto:yuri@rxmos.dev.br"
-                target="_blank"
-                className="flex flex-row items-center gap-1.5 text-xs text-muted-foreground"
-              >
-                <TbBrandMailgun /> yuri@rxmos.dev.br
-              </a>
+            <div className="flex flex-col items-start text-start gap-4 w-full">
+              <p className="tracking-[15px] text-xs">TRUSTED BY</p>
 
-              <a className="flex flex-row items-center gap-1.5 text-xs text-muted-foreground">
-                <FaMapPin /> work from anywhere
-              </a>
+              <div className="flex flex-row justify-start gap-10 items-center text-start w-full">
+                <div className="flex flex-row items-center gap-4">
+                  <div className="relative flex flex-row gap-2 w-15 h-15 rounded-tl-3xl rounded-br-3xl overflow-hidden flex-shrink-0">
+                    <Image
+                      src={vizantu}
+                      alt="vizantu Logo"
+                      fill
+                      className="object-cover saturate-0"
+                    />
+                  </div>
+                  <div>
+                    <h1 className="text-md font-bold">Vizantu</h1>
+                    <h2 className="text-xs text-muted-foreground w-fit">Social Media</h2>
+                    <p className="text-xs text-muted-foreground">Customer since 2021</p>
+                  </div>
+                </div>
+
+                <a
+                  href="https://vizantu.com.br"
+                  target="_blank"
+                  className="text-xs text-muted-foreground"
+                >
+                  <FaExternalLinkAlt />
+                </a>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-start text-start gap-3 w-full">
+              <p className="tracking-[15px] text-xs">CONTACT</p>
+              <div className="flex flex-col  items-start text-start gap-2">
+                <a
+                  href="mailto:yuri@rxmos.dev.br"
+                  target="_blank"
+                  className="flex flex-row items-center gap-1.5 text-xs text-muted-foreground"
+                >
+                  <TbBrandMailgun /> yuri@rxmos.dev.br
+                </a>
+
+                <a className="flex flex-row items-center gap-1.5 text-xs text-muted-foreground">
+                  <FaMapPin /> work from anywhere
+                </a>
+              </div>
             </div>
           </div>
         </div>
